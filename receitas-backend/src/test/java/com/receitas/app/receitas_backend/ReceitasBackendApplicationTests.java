04bf5708f1cf0f659a05bd716eaec7d838282a7a
Matheus <mrogeriopesarini@gmail.com>
2025-05-20 23:59:28 -0300

    First commit

---
+++ receitas-backend/src/test/java/com/receitas/app/receitas_backend/ReceitasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receitas.app.receitas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
